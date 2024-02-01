19fd7b377f2c9f4b368a92b1763c9346d44c3e20
BEING\inatomi <inatomi@beingcorp.co.jp>
2024-02-01 18:26:29 +0900

    先頭の大文字化と条件文整理

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,11 +1,11 @@
 for (let num = 1; num <= 20; num++) {
-  if (num % 3 === 0 && num % 5 !== 0) {
-    console.log("fizz");
-  } else if (num % 3 !== 0 && num % 5 === 0) {
-    console.log("buzz");
-  } else if (num % 3 === 0 && num % 5 === 0) {
-    console.log("fizzbuzz");
+  if (num % 3 == 0 && num % 5 == 0) {
+    console.log("FizzBuzz");
+  } else if (num % 3 === 0) {
+    console.log("Fizz");
+  } else if (num % 5 === 0) {
+    console.log("Buzz");
   } else {
-    console.log(num);
+    console.log(num);   
   }
 }
