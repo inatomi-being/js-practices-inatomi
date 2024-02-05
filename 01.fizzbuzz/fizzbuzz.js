7ae2c10bdaf238a32139fbcb7e9b1873183186c9
BEING\inatomi <inatomi@beingcorp.co.jp>
2024-02-05 14:47:06 +0900

    等価演算子と厳密等価演算子の修正

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -3,9 +3,9 @@
 for (let num = 1; num <= 20; num++)
   if (num % 3 == 0 && num % 5 == 0) {
     console.log("FizzBuzz");
-  } else if (num % 3 === 0) {
+  } else if (num % 3 == 0) {
     console.log("Fizz");
-  } else if (num % 5 === 0) {
+  } else if (num % 5 == 0) {
     console.log("Buzz");
   } else {
     console.log(num);
