cacbc8cea79ca6414e33453631a28d12f2f76f89
BEING\inatomi <inatomi@beingcorp.co.jp>
2024-02-05 15:36:20 +0900

    厳密等価演算子に変更

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
 for (let num = 1; num <= 20; num++){
-  if (num % 3 == 0 && num % 5 == 0) {
+  if (num % 3 === 0 && num % 5 === 0) {
     console.log("FizzBuzz");
-  } else if (num % 3 == 0) {
+  } else if (num % 3 === 0) {
     console.log("Fizz");
-  } else if (num % 5 == 0) {
+  } else if (num % 5 === 0) {
     console.log("Buzz");
   } else {
     console.log(num);
