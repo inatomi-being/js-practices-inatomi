f409f19b6b21bfd74620f2274167b8840fcec3e8
BEING\inatomi <inatomi@beingcorp.co.jp>
2024-02-05 16:15:20 +0900

    空白の追加

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-for (let num = 1; num <= 20; num++){
+for (let num = 1; num <= 20; num++) {
   if (num % 3 === 0 && num % 5 === 0) {
     console.log("FizzBuzz");
   } else if (num % 3 === 0) {
