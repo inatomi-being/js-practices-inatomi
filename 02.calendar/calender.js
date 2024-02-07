fcc8ece2137f4ed5e133659b853c13567fe22e98
BEING\inatomi <inatomi@beingcorp.co.jp>
2024-02-07 16:40:22 +0900

    再提出　padStartの利用　日曜日の左側の空白の削除　letからconstへの変更

--- 02.calendar/calender.js
+++ 02.calendar/calender.js
@@ -1,29 +1,25 @@
 #!/usr/bin/env node
-const today = new Date();
-const yearmonths = process.argv.slice(2);
-const year = yearmonths[0] || today.getFullYear();
-const month = yearmonths[1] || today.getMonth() + 1;
-console.log(year + "年" + month + "月");
-let weeks = ["日", "月", "火", "水", "木", "金", "土"];
-console.log(weeks.join("  "));
-let startDate = new Date(year, month - 1, 1);
-let endDate = new Date(year, month, 0);
-let startDay = startDate.getDay();
+const today = new Date()
+const yearmonths = process.argv.slice(2)
+const year = yearmonths[0] || today.getFullYear()
+const month = yearmonths[1] || today.getMonth() + 1
+console.log(year + '年' + month + '月')
+const weeks = ['日', '月', '火', '水', '木', '金', '土']
+console.log(weeks.join(' '))
+const startDate = new Date(year, month - 1, 1)
+const endDate = new Date(year, month, 0)
+const startDay = startDate.getDay()
 
 for (let i = 0; i < startDay; i++) {
-  process.stdout.write("    ");
+  process.stdout.write('   ')
 }
 for (let i = 1; i <= endDate.getDate(); i++) {
-  if (String(i).length > 1) {
-    i = i;
-  } else {
-    i = " " + i;
-  }
+  const dayString = String(i).padStart(2, ' ')
+  process.stdout.write(`${dayString} `)
 
-  process.stdout.write(` ${i} `);
-  let Day = new Date(year, month - 1, i).getDay();
+  const day = new Date(year, month - 1, i).getDay()
 
-  if (Day == 6) {
-    console.log(``);
+  if (day == 6) {
+    console.log(``)
   }
 }
